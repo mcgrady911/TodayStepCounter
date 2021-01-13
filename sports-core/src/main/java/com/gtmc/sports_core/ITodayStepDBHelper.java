7e9b489d87425dab74326b28f7cb9fe3da974812
mcgrady <mcgrady911@foxmail.com>
2021-01-13 14:17:04 +0800

    remove userId

---
+++ sports-core/src/main/java/com/gtmc/sports_core/ITodayStepDBHelper.java
@@ -0,0 +1,27 @@
+package com.gtmc.sports_core;
+
+import java.util.List;
+
+
+interface ITodayStepDBHelper {
+
+    void createTable();
+
+    void deleteTable();
+
+    void clearCapacity(String userId, String curDate, int limit);
+
+    boolean isExist(TodayStepData todayStepData);
+
+    void insert(TodayStepData todayStepData);
+
+    TodayStepData getMaxStepByDate(String userId, long millis);
+
+    List<TodayStepData> getQueryAll(String userId);
+
+    List<TodayStepData> getStepListByDate(String userId, String dateString);
+
+    List<TodayStepData> getStepListByStartDateAndDays(String userId, String startDate, int days);
+
+    void close();
+}
---
+++ sports-core/src/main/java/com/gtmc/sports_core/TodayStepShutdownReceiver.java
@@ -0,0 +1,22 @@
+package com.gtmc.sports_core;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by jiahongfei on 2017/9/27.
+ */
+
+public class TodayStepShutdownReceiver extends BroadcastReceiver {
+
+    private static final String TAG = TodayStepShutdownReceiver.class.getSimpleName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_SHUTDOWN)) {
+            TodayStepPreferencesHelper.setShutdown(context,true);
+        }
+    }
+
+}
